d926d62a954d7a8ac3d306f938bc673fcd206541
GuidoGross <profesional.gig.2802@outlook.com>
2026-03-23 22:15:47 -0300

    The project's base path has been corrected.

--- source/constants.py
+++ source/constants.py
@@ -1,5 +1,5 @@
 import os
 
-BASE_PATH = "/content"
+BASE_PATH = "/Analysis_of_the_Job_Characteristics_of_Computer_Science_Engineers_-Google_Colaboratory-"
 DATA_PATH = os.path.join(BASE_PATH, "assets", "data")
 DATA_BASE_PATH = os.path.join(DATA_PATH, "data_base.csv")
